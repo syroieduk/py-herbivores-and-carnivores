add1489ffc7bb6107394ada2dff475059b9d91fa
Yurii Syroied <syroiedyur@gmail.com>
2026-08-14 12:45:59 +0300

    Animals exercise

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(
+            self, name: str,
+            health: int = 100,
+            hidden: bool = False) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+
+        Animal.alive.append(self)
+
+    def __str__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore) -> None:
+        if isinstance(herbivore, Herbivore) is False:
+            return
+        if herbivore.hidden is True:
+            return
+        herbivore.health -= 50
+
+        if herbivore.health <= 0:
+            Animal.alive.remove(herbivore)
